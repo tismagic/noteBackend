e2824b557309fe9ae4b3ba0cadf0f7b763e496af
Tismagic <duanrancosmos@gmail.com>
2022-01-31 22:01:37 +0800

    frontandbacktogether

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 
 app.use(cors())
+app.use(express.static("build"))
 
 app.use(express.json())
 const requestLogger = (request, response, next) => {
